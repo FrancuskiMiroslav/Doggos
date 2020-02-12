781c517b27532b7b090dede107af3f92e7dac522
Francuski Miroslav <francuski.miroslav@gmail.com>
2020-02-12 19:34:01 +0100

    added favicon

--- src/js/main.js
+++ src/js/main.js
@@ -5,14 +5,14 @@ document.addEventListener("DOMContentLoaded", function() {
   let delta = 0;
 
   wrapper.addEventListener("mousemove", function(e) {
-    delta = (e.clientX - window.innerWidth / 2) * 0.5;
+    /*    delta = (e.clientX - window.innerWidth / 2) * 0.5; */
     handle.style.left = e.clientX + delta + "px";
 
     topLayer.style.width = e.clientX + delta + "px";
   });
 
   wrapper.addEventListener("touchmove", function(e) {
-    delta = (e.changedTouches[0].clientX - window.innerWidth / 2) * 0.5;
+    /* delta = (e.changedTouches[0].clientX - window.innerWidth / 2) * 0.5; */
     handle.style.left = e.changedTouches[0].clientX + delta + "px";
     topLayer.style.width = e.changedTouches[0].clientX + delta + "px";
   });
