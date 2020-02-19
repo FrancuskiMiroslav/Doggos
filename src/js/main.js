3b0abdbe3cada29b311278f96f3083ff4a816dae
Francuski Miroslav <francuski.miroslav@gmail.com>
2020-02-19 16:20:38 +0100

    added carousel with array of slides

--- src/js/main.js
+++ src/js/main.js
@@ -17,3 +17,97 @@ document.addEventListener("DOMContentLoaded", function() {
     topLayer.style.width = e.changedTouches[0].clientX + delta + "px";
   });
 });
+
+// carousel
+const nextBtn = document.querySelector("#next");
+const prevBtn = document.querySelector("#prev");
+const carousel = document.querySelector(".carousel");
+
+let firstSlide;
+let lastSlide;
+
+let slides = [
+  {
+    title: "Pera Perčina",
+    type: "sandman",
+    content: "lorem ipsum dolor",
+    image: "../images/carousel/slide1.jpg"
+  },
+  {
+    title: "Jaza Jazura",
+    type: "",
+    content: "lorem ipsum dolor",
+    image: "../images/carousel/slide2.jpg"
+  },
+  {
+    title: "Pera v Jaza",
+    type: "Čortanovci",
+    content: "lorem ipsum dolor",
+    image: "../images/carousel/slide3.jpg"
+  },
+  {
+    title: "Jaza",
+    type: "",
+    content: "lorem ipsum dolor",
+    image: "../images/carousel/slide4.jpg"
+  }
+];
+
+slides.forEach(({ title, type, content, image }, i) => {
+  const slide = document.createElement("div");
+  slide.classList.add("carousel__slide");
+  slide.style.backgroundImage = "url(" + image + ")";
+
+  if (i == 0) {
+    firstSlide = slide;
+    slide.classList.add("active");
+  } else if (i + 1 == slides.length) {
+    lastSlide = slide;
+  }
+
+  const slideContent = document.createElement("div");
+  slideContent.classList.add("carousel__content");
+
+  const contentTitle = document.createElement("h3");
+  contentTitle.classList.add("carousel__title");
+  contentTitle.textContent = title;
+
+  const contentType = document.createElement("span");
+  contentType.textContent = type;
+
+  const contentContent = document.createElement("div");
+  contentContent.classList.add("carousel__desc");
+  contentContent.textContent = content;
+
+  contentTitle.appendChild(contentType);
+  slideContent.appendChild(contentTitle);
+  slideContent.appendChild(contentContent);
+  slide.appendChild(slideContent);
+
+  carousel.appendChild(slide);
+});
+
+nextBtn.addEventListener("click", () => {
+  const activeSlide = document.querySelector(".carousel__slide.active");
+  let nextSibling = activeSlide.nextElementSibling;
+
+  if (nextSibling == null) {
+    nextSibling = activeSlide;
+  }
+
+  if (nextSibling.classList.contains("carousel__slide")) {
+    activeSlide.classList.remove("active");
+    nextSibling.classList.add("active");
+  }
+});
+
+prevBtn.addEventListener("click", () => {
+  const activeSlide = document.querySelector(".carousel__slide.active");
+  let nextSibling = activeSlide.previousElementSibling;
+  console.log(nextSibling);
+
+  if (nextSibling.classList.contains("carousel__slide")) {
+    activeSlide.classList.remove("active");
+    nextSibling.classList.add("active");
+  }
+});
